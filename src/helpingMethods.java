5be08f976571da4e6bcad61e24d029459cf01179
Farhad Rasoli <rasoolyfarhad7@gmail.com>
2024-11-23 18:08:50 -0500

    added a static function for connecting to the database

--- src/helpingMethods.java
+++ src/helpingMethods.java
@@ -4,6 +4,15 @@ import javax.swing.JPanel;
 import javax.swing.JTextField;
 
 import java.awt.Font;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import java.awt.Color;
 import javax.swing.JFrame;
 
@@ -56,4 +65,45 @@ public class helpingMethods {
         return isinteger;
     }
 
+    public static Connection establishConnection(){
+        Connection con = null;
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            String[] creds = helpingMethods.getCreds();
+            con = DriverManager.getConnection("jdbc:mysql://localhost:3306/ETS", creds[0], creds[1]);
+
+        } catch (ClassNotFoundException e) {
+            // TODO: handle exception
+            Logger.getLogger(addrecord.class.getName()).log(Level.SEVERE, null, e);
+        } catch (SQLException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+        return con;
+    }
+
+    
+    public static String[] getCreds(){
+        File file = new File("./ExpenseTracker/src/credentials.txt");
+        String[] result = new String[2];
+        String res = "";
+        try {
+            FileReader reader = new FileReader(file);
+            int data = reader.read();
+            
+            while(data != -1){
+                res += (char)data;
+                data = reader.read();
+            }
+            reader.close();
+        } catch (FileNotFoundException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        } catch (IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+        result = res.split(",");
+        return result;
+    }
 }
